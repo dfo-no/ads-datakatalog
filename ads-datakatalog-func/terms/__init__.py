43cd386718c2c5074d11e474f58e42b38ec24822
Peder Lång Skeidsvoll <pls@computas.com>
2022-02-18 15:56:55 +0100

    Nytt api-punkt: terms

--- ads-datakatalog-func/terms/__init__.py
+++ ads-datakatalog-func/terms/__init__.py
@@ -8,13 +8,32 @@ glossary_id = 'ef38fdb9-bbd5-4c5e-92fd-30bebc07a3f1'
 def main(req: func.HttpRequest) -> func.HttpResponse:
     purview_client = get_purview_client()
 
+    # Local test:
+    #
+    # with open('terms/sample.json') as json_file:
+    #     terms = json.load(json_file)
+
     terms: dict = purview_client.glossary.get_detailed_glossary(
         glossary_id, include_term_hierarchy=True)
 
-    term_info: list = terms.get('termInfo')
+    term_info: dict = terms.get('termInfo')
 
-    approved_terms = list(
-        filter(lambda t: t.Get('status') == 'Approved', term_info))
+    # Fjern alle ikke approved termer
+    terms_flattened = map(lambda t: term_info.get(t), term_info)
+    approved_terms = list(filter(
+        lambda t: t.get('status') == 'Approved',
+        terms_flattened))
+
+    # Fjern alle relaterte termer som ikke er approved
+    approved_guids = list(map(lambda t: t.get('guid'), approved_terms))
+    for term in approved_terms:
+        related_terms = term.get('seeAlso')
+        approved_related_terms = []
+        for related_term in related_terms:
+            related_term_guid = related_term.get('termGuid')
+            if related_term_guid in approved_guids:
+                approved_related_terms.append(related_term)
+        term['seeAlso'] = approved_related_terms
 
     return func.HttpResponse(
         json.dumps(approved_terms),
