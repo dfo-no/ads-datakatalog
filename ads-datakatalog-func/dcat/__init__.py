fe463eb597f06fada5087234ada6f79a66f34af3
Jeff Reiffers <jeffreiffers@gmail.com>
2022-11-29 09:58:47 +0100

    feat: use atlasdcat to map glossary terms to rdf

--- ads-datakatalog-func/dcat/__init__.py
+++ ads-datakatalog-func/dcat/__init__.py
@@ -1,15 +1,20 @@
 import azure.functions as func
-from helpers.dcat_mapper.mapper import map_json_to_rdf
-from helpers.purview.purview_service import get_terms
-
-glossary_id = 'ef38fdb9-bbd5-4c5e-92fd-30bebc07a3f1'
-
+from helpers.dcat.dcat_service import get_dcat
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    terms = get_terms(False, True)
 
-    return func.HttpResponse(
-        map_json_to_rdf(terms),
-        status_code=200,
-        mimetype='text/turtle'
-    )
+    try:
+        return func.HttpResponse(
+            get_dcat(),
+            status_code=200,
+            mimetype='text/turtle'
+        )
+    except Exception as e:
+        print(f"An exception occurred: {e}")
+        return func.HttpResponse(
+            'An exception occurred',
+            status_code=500,
+            mimetype='text/plain'
+        )
+        
+
